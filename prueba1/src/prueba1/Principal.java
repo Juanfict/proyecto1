9f2dd3d150b98b335159b5481438d9596d09f0ac
juanotravez <juan2@esotroejemplo.es>
2021-03-09 09:24:47 +0100

    Segundo commit

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,5 +3,6 @@ package prueba1;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo. Java Git.");
+		System.out.println("Hola mundo. Java Git.");
 	}
 }
