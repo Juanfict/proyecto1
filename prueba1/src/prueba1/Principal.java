d5b690e53a228a05582cc2260ce3097075277aaf
juanotravez <juan2@esotroejemplo.es>
2021-03-09 13:12:59 +0100

    Cambio en principal, sobre rama.

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo. Java Git.");
 		System.out.println("Hola mundo. Java Git.");
+		System.out.println("Pruebas Git. Se añade cambio1.");
 	}
 }
